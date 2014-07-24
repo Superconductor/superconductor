3d42ee51e1e99943b4f12e127672f860c8a01306
Leo Meyerovich <leo@graphistry.com>
2014-07-24 10:16:24 -0700

    Batched read/writes

--- superconductorjs/src/CLDataWrapper.js
+++ superconductorjs/src/CLDataWrapper.js
@@ -18,6 +18,13 @@ function CLDataWrapper(clr, hostBuffer, clBuffer) {
 	if (clr.cfg.ignoreCL) {
 		this.get = function(index) { return hostBuffer[index]; };
 		this.set = function(index, value) { hostBuffer[index] = value; return value; };
+		this.setBatched = function (index, view) {
+            var typed = view instanceof hostBuffer.constructor ?  view : new hostBuffer.constructor(view);
+            if (typed.length > hostBuffer.length - index) {
+                clr.console.debug('batched write clamped');
+            }
+            hostBuffer.set(typed.subarray(0, Math.min(typed.length, hostBuffer.length - index)), index);
+        };
 		this.__defineGetter__("length", function() { return hostBuffer.length; });
 	} else {
 
@@ -26,26 +33,75 @@ function CLDataWrapper(clr, hostBuffer, clBuffer) {
 			// Create a new typed array of the same type as hostBuffer, 1 element long
 			var target = new hostBuffer.constructor(1);
 			var itemOffset = hostBuffer.byteOffset + (hostBuffer.BYTES_PER_ELEMENT * index);
-	
+
 			clr.queue.enqueueReadBuffer(clBuffer, true, itemOffset, target.byteLength, target);
-	
+
 			return target[0];
 		};
-	
-	
+
+        //read starting at index up to shorter of destination array and amount
+        // int * int -> [ 'a ]
+        // int * [ 'a ] -> [ 'a ]
+        // int * [ 'a ] * int -> [ 'a ]
+        this.getBatched = function (index, amountOrArray, maybeAmount) {
+
+            var suggestedAmount =
+                typeof maybeAmount == 'number' ? maybeAmount
+                : typeof amountOrArray == 'object' ? amountOrArray.length
+                : amountOrArray;
+
+            var itemOffset = hostBuffer.byteOffset + (hostBuffer.BYTES_PER_ELEMENT * index);
+
+            var actualAmount = Math.min(
+                suggestedAmount,
+                typeof amountOrArray == 'object' ? amountOrArray.length : suggestedAmount,
+                (hostBuffer.byteLength - itemOffset) / hostBuffer.BYTES_PER_ELEMENT);
+
+            if (!actualAmount) {
+                return console.error('empty write');
+            }
+
+            var target = typeof amountOrArray == 'object' ? amountOrArray : new hostBuffer.constructor(actualAmount);
+/*            console.error('suggested', suggestedAmount * Float32Array.BYTES_PER_ELEMENT,
+                'actual', actualAmount  * Float32Array.BYTES_PER_ELEMENT,
+                'offset', itemOffset,
+                'target len', target.byteLength)
+*/
+            clr.queue.enqueueReadBuffer(
+                clBuffer, true,
+                itemOffset, actualAmount * Float32Array.BYTES_PER_ELEMENT,
+                target);
+
+            return target;
+        }
+
+
 		this.set = function(index, value) {
 			// Create a new typed array of the same type as hostBuffer, 1 element long
 			var target = new hostBuffer.constructor(1);
 			target[0] = value;
 			var itemOffset = hostBuffer.byteOffset + (hostBuffer.BYTES_PER_ELEMENT * index);
-	
+
 			clr.queue.enqueueWriteBuffer(clBuffer, true, itemOffset, target.byteLength, target);
-	
+
 			return value;
 		};
-	
-	
+
+        //write from index to shorter of end of array or end of view
+		this.setBatched = function (index, view) {
+            if (!view.length) return;
+            var typed = view instanceof hostBuffer.constructor ?  view : new hostBuffer.constructor(view);
+            var itemOffset = hostBuffer.byteOffset + hostBuffer.BYTES_PER_ELEMENT * index;
+            clr.queue.enqueueWriteBuffer(clBuffer, true, itemOffset, typed.byteLength, typed);
+            return typed;
+        };
+
+
 		// The size of this buffer in number of elements
 		this.__defineGetter__("length", function() { return hostBuffer.length });
 	}
+}
+
+if (typeof(module) != 'undefined') {
+    module.exports.CLDataWrapper = CLDataWrapper;
 }
