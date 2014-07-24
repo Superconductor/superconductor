d8163d5106683faec0bc225a5d1ceb274acc317f
Leo Meyerovich <leo@graphistry.com>
2014-07-24 10:11:00 -0700

    Update example server to allow cross-origin

--- runTestWebServer.sh
+++ runTestWebServer.sh
@@ -1,8 +1,17 @@
-#!/usr/bin/env bash
+#! /usr/bin/env python
 
 # Script to use python to run a simple, basic webserver suitable for using to test Superconductor.
 # Superconductor needs to be loaded from a web server because it uses AJAX requests to fetch data.
 # Due to security restrictions in the browser, these requests are not allowed to target local files,
 # even if the requesting page is also local.
 
-python -m SimpleHTTPServer 8888
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+import BaseHTTPServer
+
+class CORSRequestHandler (SimpleHTTPRequestHandler):
+    def end_headers (self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        SimpleHTTPRequestHandler.end_headers(self)
+
+if __name__ == '__main__':
+    BaseHTTPServer.test(CORSRequestHandler, BaseHTTPServer.HTTPServer)
